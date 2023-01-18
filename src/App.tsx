3af998efb2dab7fb4dc92da89286ee8a813e86a4
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-01-18 19:41:13 +0500

    to-do-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Panel from './components/Panel/Panel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Panel />
     </div>
   );
 }
---
+++ src/components/Panel/Panel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Panel.css'
+
+function Panel() {
+  return (
+    <div className='panel'>
+      <p>123</p>
+    </div>
+  );
+}
+
+export default Panel;
