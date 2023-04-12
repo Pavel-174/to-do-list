00251cf39d9c31f29a53bac8e8b256ccfff4469f
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-04-12 12:32:41 +0500

    Localstorage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,28 +6,38 @@ import Footer from '../Footer/Footer';
 import './App.css';
 
 export type Task = {
-      id: number;
-      name: string;
-      description: string;
-      checked: boolean;
+      id: number | null;
+      name: any;
+      description: any;
+      checked: boolean | null;
 }
 
 function App() {
 
   const [edit, setEdit] = React.useState <number | null> (null);
 
-  const [tasks, setTasks] = React.useState([
-    {id: 1, name: 'test1', description: 'dfngnlkasdm;l', checked: false},
-    {id: 2, name: 'test2', description: 'dfngnlkasdm;l', checked: false},
-    {id: 3, name: 'Test3', description: 'dfngnlkasdm;l', checked: true}
-  ]);
+  // const [tasks, setTasks] = React.useState([
+  //   {id: 1, name: 'test1', description: 'dfngnlkasdm;l', checked: false},
+  //   {id: 2, name: 'test2', description: 'dfngnlkasdm;l', checked: false},
+  //   {id: 3, name: 'Test3', description: 'dfngnlkasdm;l', checked: true}
+  // ]);
+  
+  const [tasks, setTasks] = React.useState(() => {
+    const saved: any = localStorage.getItem("tasks");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function editTask (id: Task['id']) {
     setEdit(id)
   };
 
   function deleteTask (id: Task['id']) {
-    setTasks(tasks.filter(task => task.id !== id))
+    setTasks(tasks.filter((task: { id: number | null; }) => task.id !== id))
   };
 
   function addTask ({name, description}: Omit<Task, 'id' | 'checked'>) {
@@ -35,7 +45,7 @@ function App() {
   }
 
   function checkTask (id: Task['id']) {
-    setTasks(tasks.map((task) => {
+    setTasks(tasks.map((task: { id: number | null; checked: any; }) => {
       if(task.id === id) {
         return { ...task, checked: !task.checked };
       }
@@ -45,7 +55,7 @@ function App() {
 
   const changeTask = ({ name, description }: Omit<Task, 'id' | 'checked'>) => {
     setTasks(
-      tasks.map((task) => {
+      tasks.map((task: { id: number | null; }) => {
         if (task.id === edit) {
           return { ...task, name, description };
         }
